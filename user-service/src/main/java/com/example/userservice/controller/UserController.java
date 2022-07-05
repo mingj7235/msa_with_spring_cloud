5515b18256d0ba31f37fa9d46b32e047b0d2272f
김민재 <joshuara7235@gmail.com>
2022-07-05 23:06:45 +0900

    ModelMapper 활용 Response 에 httpStatus 코드 담아서 보내기 Request -> Dto -> Entity 순환 모델

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -4,11 +4,14 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.serivce.UserService;
 import com.example.userservice.vo.Greeting;
 import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -36,13 +39,14 @@ public class UserController {
     }
 
     @PostMapping ("/users")
-    public String createUser(@RequestBody RequestUser requestUser) {
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser requestUser) {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserDto userDto = mapper.map(requestUser, UserDto.class);
         userService.createUser(userDto);
 
-        return "Create user method is called";
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(mapper.map(userDto, ResponseUser.class));
     }
 }
--- user-service/src/main/java/com/example/userservice/serivce/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/serivce/UserServiceImpl.java
@@ -29,7 +29,7 @@ public class UserServiceImpl implements UserService{
 
         userRepository.save(userEntity);
 
-        return null;
+        return mapper.map(userEntity, UserDto.class);
     }
 
 }
