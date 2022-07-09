96927d6f58d853f134f726cf1b03ad17001c2681
김민재 <joshuara7235@gmail.com>
2022-07-09 17:34:41 +0900

    Catalog service basic structure 생성 entity, repository, dto, response vo

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-O01', "Berlin", 100, 1500);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-O02', "Tokyo", 110, 1000);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-O03', "Stockholm", 120, 2000);
