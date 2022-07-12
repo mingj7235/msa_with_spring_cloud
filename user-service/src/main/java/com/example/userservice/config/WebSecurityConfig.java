052c0dab2ba8a02818f93b3ff5c7c0b7ec2dab98
김민재 <joshuara7235@gmail.com>
2022-07-12 22:11:02 +0900

    Spring security 설정 - User service login 기능

--- user-service/src/main/java/com/example/userservice/config/WebSecurityConfig.java
+++ user-service/src/main/java/com/example/userservice/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.userservice.config;
 
+import com.example.userservice.Filter.AuthenticationFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,9 +14,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity http) throws Exception {
 
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**");
+//                .hasIpAddress("") // <- IP
+        http.addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager()); //Spring Security 가 제공하는 인증 매니져
+
+        return authenticationFilter;
     }
 
 }
