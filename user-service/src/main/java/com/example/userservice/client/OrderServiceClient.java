a9a121ac60ae6926de4e8d2cd68368a34282529a
joshua <joshuara7235@gmail.com>
2022-08-02 22:24:20 +0900

    Feign Client order api endpoint added

--- user-service/src/main/java/com/example/userservice/client/OrderServiceClient.java
+++ user-service/src/main/java/com/example/userservice/client/OrderServiceClient.java
@@ -2,8 +2,10 @@ package com.example.userservice.client;
 
 import com.example.userservice.vo.ResponseOrder;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -13,4 +15,11 @@ public interface OrderServiceClient {
     @GetMapping ("/order-service/{userId}/orders") // Order-Service 에 있는 endpoint
 //    @GetMapping ("/order-service/{userId}/orders_ng") // 예외처리 테스트를 위한 Endpoint 를 일부러 잘못 입력
     List<ResponseOrder> getOrders(@PathVariable final String userId);
+
+    @PostMapping ("/order-service/{userId}/orders")
+    ResponseOrder createOrders (@PathVariable final String userId);
+
+    @DeleteMapping ("/order-service/{userId}/orders/{orderId}")
+    void deleteOrders (@PathVariable final String userId,
+                       @PathVariable final String orderId);
 }
