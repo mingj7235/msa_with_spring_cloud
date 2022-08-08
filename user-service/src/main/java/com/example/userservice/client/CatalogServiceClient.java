a3309ea71fb790fc95bfd0f0c14b6c0ca1687562
joshua <joshuara7235@gmail.com>
2022-08-08 22:46:22 +0900

    Get Catalog post method for feign client

--- user-service/src/main/java/com/example/userservice/client/CatalogServiceClient.java
+++ user-service/src/main/java/com/example/userservice/client/CatalogServiceClient.java
@@ -1,9 +1,12 @@
 package com.example.userservice.client;
 
+import com.example.userservice.dto.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @FeignClient (name = "catalog-service")
@@ -11,4 +14,7 @@ public interface CatalogServiceClient {
 
     @GetMapping ("/catalog-service/{userId}")
     List<String> getCatalogNameList (@PathVariable String userId);
+
+    @PostMapping ("/catalog-service/")
+    String saveCatalogName (final @NotNull UserDto request);
 }
