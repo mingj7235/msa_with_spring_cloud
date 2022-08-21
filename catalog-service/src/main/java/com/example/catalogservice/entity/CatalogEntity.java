8451490291bc9359867c022d8a923af94589c2a3
joshua <joshuara7235@gmail.com>
2022-08-21 23:25:19 +0900

    Entity capsulize method pattern on toEntity

--- catalog-service/src/main/java/com/example/catalogservice/entity/CatalogEntity.java
+++ catalog-service/src/main/java/com/example/catalogservice/entity/CatalogEntity.java
@@ -1,14 +1,19 @@
 package com.example.catalogservice.entity;
 
-import lombok.Data;
+import com.example.catalogservice.dto.CatalogDto;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
+@AllArgsConstructor (access = AccessLevel.PRIVATE)
 @Table (name = "catalog")
 public class CatalogEntity implements Serializable {
 
@@ -31,4 +36,11 @@ public class CatalogEntity implements Serializable {
     @Column (nullable = false, updatable = false, insertable = false)
     @ColumnDefault(value = "CURRENT_TIMESTAMP")
     private LocalDateTime createdAt;
+
+    public static CatalogEntity toEntity (final @NotNull CatalogDto request) {
+        return CatalogEntity.builder()
+                .productId(request.getProductId())
+                .unitPrice(request.getUnitPrice())
+                .build();
+    }
 }
--- order-service/src/main/java/com/example/orderservice/entity/OrderEntity.java
+++ order-service/src/main/java/com/example/orderservice/entity/OrderEntity.java
@@ -1,14 +1,19 @@
 package com.example.orderservice.entity;
 
-import lombok.Data;
+import com.example.orderservice.dto.OrderDto;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 @Entity
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
+@AllArgsConstructor (access = AccessLevel.PRIVATE)
 @Table (name = "orders")
 public class OrderEntity implements Serializable {
     @Id
@@ -36,4 +41,15 @@ public class OrderEntity implements Serializable {
     @Column (nullable = false, updatable = false, insertable = false)
     @ColumnDefault(value = "CURRENT_TIMESTAMP")
     private LocalDateTime createdAt;
+
+    public static OrderEntity toEntity (final @NotNull OrderDto request) {
+        return OrderEntity.builder()
+                .productId(request.getProductId())
+                .qty(request.getQty())
+                .unitPrice(request.getUnitPrice())
+                .totalPrice(request.getTotalPrice())
+                .userId(request.getUserId())
+                .orderId(request.getOrderId())
+                .build();
+    }
 }
