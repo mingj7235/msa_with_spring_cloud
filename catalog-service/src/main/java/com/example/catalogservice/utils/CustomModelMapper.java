6e78f7ff651101415a8129a6e23e2186dd89b174
joshua <joshuara7235@gmail.com>
2022-08-27 20:16:55 +0900

    Modelmapper bean 등록

---
+++ catalog-service/src/main/java/com/example/catalogservice/utils/CustomModelMapper.java
@@ -0,0 +1,33 @@
+package com.example.catalogservice.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomModelMapper {
+
+    private final ModelMapper modelMapper;
+
+    @Bean
+    public ModelMapper strictMapper () {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+
+    @Bean
+    public ModelMapper looseMapper () {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
+    }
+
+    @Bean
+    public ModelMapper standardMapper () {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STANDARD);
+        return modelMapper;
+    }
+
+}
